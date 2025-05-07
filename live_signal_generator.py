92dc5b59cf7f43af166e60b71c8ddb56088a1371
jsingh130 <jagsinghjr@gmail.com>
2025-05-07 00:44:12 -0400

    Update live_signal_generator.py

--- live_signal_generator.py
+++ live_signal_generator.py
@@ -20,6 +20,7 @@ sheet_service = build("sheets", "v4", credentials=credentials)
 
 # Download SPX 5-min data
 df = yf.download("^GSPC", interval="5m", period="1d")
+close = df["Close"].values.flatten()
 df["EMA9"] = EMAIndicator(df["Close"], 9).ema_indicator()
 df["EMA21"] = EMAIndicator(df["Close"], 21).ema_indicator()
 df["ATR"] = AverageTrueRange(df["High"], df["Low"], df["Close"]).average_true_range()
